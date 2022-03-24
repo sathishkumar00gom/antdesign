4be86ef35380254bfc2c489fd22a11412e4a23c4
sathishkumar00gom <sathishkumarm@dorustree.in>
2022-03-24 19:30:08 +0530

    use Effect Changes

--- src/E-Commerce/Ecom.js
+++ src/E-Commerce/Ecom.js
@@ -1,35 +1,75 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useState } from "react";
 import {
   Navbar, Nav, Form, InputGroup,
   FontAwesomeIcon, Carousel, Card, Button, Row, Col
 } from "react-bootstrap";
 import { ArrowRight, Laptop } from 'react-bootstrap-icons';
 import Tabletime from "./Table"
+import { Modal } from "react-bootstrap"
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'Add':
+      console.log("up", state)
+      return [...state, action.payload]
+    default:
+      return state
+    case "Delete":
+      return state.filter((product) => product.id !== action.payload)
 
+    case "edit":
+      console.log("up", state)
+      return state
 
-const Ecom = () => {
-
-  const reducer = (state,action) =>{
-    switch (action.type){
-        case 'Add':
-        return{...state, action.payload}
-    
-    case 'edit':
-    return{...[], action.payload}
-    case "delete":
-    return{...[], action.payload}
-    }
+    // case 'edit':
+    // return{...[], action.payload}
+    // case "delete":
+    // return{...[], action.payload}
+  }
 
 }
 
 
-  const HandleSubmit=(items)=>{
-    dispatch({type:add, payload:}),
-    dispatch ({type:toggle, payload:}),
-    dispatch ({type:delete, payload:}),
+const Ecom = () => {
+  //use state
+  const [reduerData, dispatch] = useReducer(reducer, [])
+  const [show, setShow] = useState(false)
+  const [pass, setPass] = useState({})
+  const [data, setData] = useState({
+    id: 1,
+    name: pass.name,
+    price: pass.price
+  })
 
+  const handleinit = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value })
   }
+  const handleUpdate = () => {
+    console.log('update data', data)
+    dispatch({ type: "edit", payload: data })
+  }
+  const handleClose = () => {
+    setShow(false)
+  }
+  const handleOpen = () => {
+    setShow(true)
+  }
+
+  const HandleDelete = (item, e) => {
+    e.preventDefault()
+
+    console.log("item", item)
+    dispatch({ type: 'Delete', payload: item.id })
+  }
+  const HandleSubmit = (item) => {
+    dispatch({ type: 'Add', payload: item })
+  }
+  const HandleEdit = (item, e) => {
+    console.log("edited=>", item)
+    handleOpen();
+    setPass(item)
+  }
+
 
   const product = [
 
@@ -89,8 +129,10 @@ const Ecom = () => {
 
 
 
+
+
   return (
-  <>
+    <>
       <Navbar bg="dark" variant="dark">
 
         <Navbar.Brand>Amazon</Navbar.Brand>
@@ -100,8 +142,6 @@ const Ecom = () => {
           <Nav.Link href="#pricing">Pricing</Nav.Link>
         </Nav>
         <Form.Control type="text" placeholder="Search" />
-
-
 
       </Navbar>
 
@@ -140,44 +180,83 @@ const Ecom = () => {
         <Row>
           <Col xs={6}>
             <Row>
-              <Col xs={12} sm={12} Me={8} xl={6} xxl={6}>
-                
+              <Col xs={12} sm={12} md={8} lg={12} xl={12} xxl={12}>
 
-          {
-            product.map((item)=>{
-              return (
-                <>
-                <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={item.url}/>
-                  <Card.Body>
-                    <Card.Title>{item.id}</Card.Title>
-                    <Card.Text>
-                      {item.name}
-                    </Card.Text>
-                    <p>{item.Price}</p>
-                    <Button onclick={()=> HandleSubmit(item)}variant="primary">Add to Buy</Button>
-                  </Card.Body>
-                </Card>
-                
-                </>
-              )
-            })
-          }
-                
-                
-              </Col>
-              
-              </Row>
+
+                {
+                  product.map((item) => {
+                    return (
+                      <>
+                        <Card style={{ width: '18rem' }}>
+                          <Card.Img variant="top" src={item.url} />
+                          <Card.Body>
+                            <Card.Title>{item.id}</Card.Title>
+                            <Card.Text>
+                              {item.name}
+                            </Card.Text>
+                            <p>{item.Price}</p>
+                            <Button onClick={() => HandleSubmit(item)} variant="primary">Add to Buy</Button>
+                            <Button onClick={(e) => HandleDelete(item, e)} variant="danger">Delete</Button>
+                            <Button onClick={(e) => HandleEdit(item, e)} variant="success">edit</Button>
+                            <Modal show={show} onHide={handleClose}>
+                              <Modal.Header closeButton>
+                                <Modal.Title>Product Details</Modal.Title>
+                              </Modal.Header>
+                              <Modal.Body>
+                                <Form>
+                                  <Form.Label>id</Form.Label>
+                                  <Form.Label>Product Name</Form.Label>
+                                  <Form.Control name="name" type="text" value={data.name} onChange={handleinit} placeholder="Product Name"></Form.Control>
+                                  <Form.Label>Product Price</Form.Label>
+                                  <Form.Control name="price" value={data.price} onChange={handleinit} type="number" placeholder="Product Price"></Form.Control>
+                                </Form>
+                              </Modal.Body>
+                              <Modal.Footer>
+                                <Button variant="secondary" onClick={handleClose}>
+                                  Close
+                                </Button>
+                                <Button variant="primary" onClick={handleUpdate}>
+                                  Update
+                                </Button>
+                              </Modal.Footer>
+                            </Modal>
+
+
+                          </Card.Body>
+                        </Card>
+
+                      </>
+                    )
+                  })
+                }
+
+
               </Col>
             </Row>
-          </div>
-   </>
+
+          </Col>
+      
+
+
+          <Col xs={6}>
+            <Row>
+              <Col xs={12} sm={12} md={8} xl={6} xxl={6}>
+                <Tabletime reduerData={reduerData} />
+
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+
+
+      </div>
+    </>
   )
 }
-        export default Ecom
+export default Ecom
 
 
-        {/* <Col xs={12} sm={12} Me={8} xl={6} xxl={6} >
+{/* <Col xs={12} sm={12} Me={8} xl={6} xxl={6} >
               <Card style={{ width: '18rem' }}>
                 <Card.Img variant="top" src=" />
                 <Card.Body>
@@ -278,7 +357,7 @@ const Ecom = () => {
         </Col>
       </Row> */}
 
-        {/* </Products> */}
+{/* </Products> */ }
 
 
 
